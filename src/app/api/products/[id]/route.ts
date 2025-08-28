21196e19008e361065b0ae848690ca2683a4d93b
Pranoy Biswas <impranoybiswas@gmail.com>
2025-08-28 12:35:05 +0600

    new product

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,29 +1,27 @@
 // src/app/api/products/[id]/route.ts
-import { getDatabase } from "@/lib/mongodb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
-
-interface Params {
-  params: { id: string };
-}
+import { getDatabase } from "@/lib/mongodb";
 
 export async function GET(
-  request: Request,
-  { params }: Params
+  request: NextRequest,
+  context: { params: { id: string } }
 ) {
+  const { id } = context.params;
+
   try {
     const db = await getDatabase();
     const product = await db
       .collection("products")
-      .findOne({ _id: new ObjectId(params.id) });
+      .findOne({ _id: new ObjectId(id) });
 
     if (!product) {
       return NextResponse.json({ error: "Product not found" }, { status: 404 });
     }
 
     return NextResponse.json(product);
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err);
     return NextResponse.json(
       { error: "Failed to fetch product" },
       { status: 500 }
